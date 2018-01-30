959f90aa85f36e49f56f89a5cb563da647e4bcc7
marco <marco.esposito.work@gmail.com>
2018-01-30 15:13:16 +0100

    label encoding

--- python/dataset_info.py
+++ python/dataset_info.py
@@ -4,21 +4,24 @@ datasets = {
 		"X_col" : range(33),
 		"Y_col" : [33],
 		"has_header" : True,
-		"filetype" : "CSV"
+		"filetype" : "CSV",
+		"encode_labels" : True
 	},
 	"bank" : {
 		"train_name" : "prep_data/bankruptcy/bankrupt.arff",
 		"X_col" : range(6),
 		"Y_col" : [6],
 		"has_header" : False,
-		"filetype" : "arff"
+		"filetype" : "arff",
+		"encode_labels" : True
 	},
 	"contraceptive" : {
 		"train_name" : "prep_data/contraceptive/contraceptive.csv",
 		"X_col" : range(9),
 		"Y_col" : [9],
 		"has_header" : False,
-		"filetype" : "CSV"
+		"filetype" : "CSV",
+		"encode_labels" : False
 	}
 
 }
