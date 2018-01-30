64834a39b046517d5e2aa492896a5d5cbaeafee1
Adnan Begoli <e1027977@student.tuwien.ac.at>
2018-01-30 16:17:22 +0100

    Data sets preprocessed

--- python/dataset_info.py
+++ python/dataset_info.py
@@ -4,21 +4,87 @@ datasets = {
 		"X_col" : range(33),
 		"Y_col" : [33],
 		"has_header" : True,
-		"filetype" : "CSV"
-	},
-	"bank" : {
-		"train_name" : "prep_data/bankruptcy/bankrupt.arff",
-		"X_col" : range(6),
-		"Y_col" : [6],
-		"has_header" : False,
-		"filetype" : "arff"
+		"filetype" : "CSV",
+		"encode_labels" : False
 	},
 	"contraceptive" : {
 		"train_name" : "prep_data/contraceptive/contraceptive.csv",
 		"X_col" : range(9),
 		"Y_col" : [9],
 		"has_header" : False,
-		"filetype" : "CSV"
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"autism" : {
+		"train_name" : "prep_data/Autism-Adult-Data/Autism-Adult-Data-preproc.csv",
+		"X_col" : range(20),
+		"Y_col" : [20],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"bankruptcy" : {
+		"train_name" : "prep_data/bankruptcy/bankrupt.csv",
+		"X_col" : range(6),
+		"Y_col" : [6],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"breast_cancer" : {
+		"train_name" : "prep_data/breast-cancer/breast-cancer-wisconsin.data",
+		"X_col" : range(9),
+		"Y_col" : [9],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"horse" : {
+		"train_name" : "prep_data/horse-colic/horse-colic.data-preproc.csv",
+		"X_col" : range(22),
+		"Y_col" : [23],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+    "hr" : {
+		"train_name" : "prep_data/hr_analytics/HR_comma_sep.csv",
+		"X_col" : range(9),
+		"Y_col" : [9],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"bankruptcy" : {
+		"train_name" : "prep_data/Qualitative_Bankruptcy/Qualitative_Bankruptcy.data.txt",
+		"X_col" : range(6),
+		"Y_col" : [6],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"english" : {
+		"train_name" : "prep_data/teaching-english/tae.csv",
+		"X_col" : range(5),
+		"Y_col" : [5],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"phishing" : {
+		"train_name" : "prep_data/website-phishing/PhishingData.csv",
+		"X_col" : range(9),
+		"Y_col" : [9],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
+	},
+	"wine" : {
+		"train_name" : "prep_data/wine-quality/winequality-white.csv",
+		"X_col" : range(11),
+		"Y_col" : [11],
+		"has_header" : True,
+		"filetype" : "CSV",
+		"encode_labels" : False
 	}
-
 }
